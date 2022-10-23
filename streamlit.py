8176df63321a4baa3ae3ef5fe97d410d63320b44
virender vashisht <77716898+vashisht9474@users.noreply.github.com>
2022-10-24 00:50:50 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -28,8 +28,9 @@ def check_password():
         st.text_input("Username", on_change=password_entered, key="username")
         st.text_input(
             "Password", type="password", on_change=password_entered, key="password"
+            st.error("😕 User not known or password incorrect")
         )
-        st.error("😕 User not known or password incorrect")
+        
         return False
     else:
         # Password correct.
