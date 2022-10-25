b9c619a76e42d87a00df8b973c904b273363e536
virender vashisht <77716898+vashisht9474@users.noreply.github.com>
2022-10-25 12:00:59 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -38,5 +38,19 @@ def check_password():
         return True
 
 if check_password():
-    st.write("Here goes your normal Streamlit app...")
-    st.button("Click me")
+    import cv2
+    import streamlit as st
+
+    st.title("Webcam Live Feed")
+    run = st.checkbox('Run')
+    FRAME_WINDOW = st.image([])
+    camera = cv2.VideoCapture(0)
+
+    while run:
+        _, frame = camera.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        FRAME_WINDOW.image(frame)
+    else:
+        st.write('Stopped')
+    
+    
