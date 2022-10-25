3f69edcce440de2e045a991eba64fa8e4144e44e
virender vashisht <77716898+vashisht9474@users.noreply.github.com>
2022-10-26 00:02:23 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -101,6 +101,30 @@ if check_password():
 		</video>
         """
 st.markdown(video_html1, unsafe_allow_html=True)
+import streamlit as st
+
+def main_page():
+    st.markdown("# Main page 🎈")
+    st.sidebar.markdown("# Main page 🎈")
+
+def page2():
+    st.markdown("# Page 2 ❄️")
+    st.sidebar.markdown("# Page 2 ❄️")
+
+def page3():
+    st.markdown("# Page 3 🎉")
+    st.sidebar.markdown("# Page 3 🎉")
+
+page_names_to_funcs = {
+    "Main Page": main_page,
+    "Page 2": page2,
+    "Page 3": page3,
+}
+
+selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+page_names_to_funcs[selected_page]()
+    
+
     
 
   
