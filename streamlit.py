0a697d71fc189593f6816816bad1c5a086d3a909
virender vashisht <77716898+vashisht9474@users.noreply.github.com>
2022-10-25 17:39:50 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -73,6 +73,33 @@ def check_password():
         return True
 
 if check_password():
+	video_html1 = """
+		<style>
+
+		#myVideo {
+		  position: fixed;
+		  right: 0;
+		  bottom: 0;
+		  min-width: 100%; 
+		  min-height: 100%;
+		}
+
+		.content {
+		  position: fixed;
+		  bottom: 0;
+		  background: rgba(0, 0, 0, 0.5);
+		  color: #f1f1f1;
+		  width: 100%;
+		  padding: 20px;
+		}
+
+		</style>	
+		<video autoplay muted loop id="myVideo">
+		  <source src="https://static.videezy.com/system/resources/previews/000/047/870/original/Text_01a_2.mp4")>
+		  Your browser does not support HTML5 video.
+		</video>
+        """
+st.markdown(video_html1, unsafe_allow_html=True)
     
 
   st.write("hello")
