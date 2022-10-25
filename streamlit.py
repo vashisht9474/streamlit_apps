ff4bbfa24517eebfe73969023cb5cf8afc2bc6f4
virender vashisht <77716898+vashisht9474@users.noreply.github.com>
2022-10-25 16:30:04 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -33,10 +33,11 @@ video_html = """
    
 
 def check_password():
+	st.markdown(video_html, unsafe_allow_html=True)
     """Returns `True` if the user had a correct password."""
 
     def password_entered():
-	st.markdown(video_html, unsafe_allow_html=True)
+	
 		
 
         """Checks whether a password entered by the user is correct."""
